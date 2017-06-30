8bd0d6629e16ad967372cc0969d3fd2f8404794c
teresaliu20 <teresali@usc.edu>
2017-06-30 14:47:35 -0700

    Todo List basics

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,5 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<p>Rewaefecacdsdsacvresvevvract lives!</p>,
+var dummyData = ["mow the lawn", "buy groceries","eat food"]
+
+class Todo extends React.Component {
+	constructor(props) {
+		super(props)
+	}
+	render() {
+		return <li><button>X</button>{this.props.task}</li>
+	}
+}
+
+class TodoList extends React.Component {
+	constructor(props) {
+		super(props)
+	}
+	render() {
+		return <ul>{dummyData.map(task => <Todo task={task}/>)}</ul>
+	}
+}
+
+ReactDOM.render(<TodoList />,
 	document.getElementById('root'))
+
